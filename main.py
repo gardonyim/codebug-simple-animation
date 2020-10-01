4a1a1931ed7e09ee096d7662c8bbe31b81476d91
Márk <mark.gardonyi@gmail.com>
2020-10-01 14:13:54 +0200

    Remove outcommented lines

--- main.py
+++ main.py
@@ -18,37 +18,21 @@ def display(line, sleep_time):
 
 
 def set_main_diagonal(sleep_time):
-    # codebug.clear()
-    # codebug.set_pixel(0, 0, 1)
-    # codebug.set_pixel(1, 1, 1)
-    # codebug.set_pixel(2, 2, 1)
-    # codebug.set_pixel(3, 3, 1)
-    # codebug.set_pixel(4, 4, 1)
     line = [(index, index, 1) for index in range(SCREEN_SIZE)]
     display(line, sleep_time)
 
 
 def set_side_diagonal(sleep_time):
-    # codebug.clear()
-    # codebug.set_pixel(0, 4, 1)
-    # codebug.set_pixel(1, 3, 1)
-    # codebug.set_pixel(2, 2, 1)
-    # codebug.set_pixel(3, 1, 1)
-    # codebug.set_pixel(4, 0, 1)
     line = [(index, SCREEN_SIZE - index, 1) for index in range(SCREEN_SIZE)]
     display(line, sleep_time)
 
 
 def set_vertical(sleep_time):
-    # codebug.clear()
-    # codebug.set_col(2, 0b11111)
     line = [(index, 2, 1) for index in range(SCREEN_SIZE)]
     display(line, sleep_time)
 
 
 def set_horizontal(sleep_time):
-    # codebug.clear()
-    # codebug.set_row(2, 0b11111)
     line = [(2, index, 1) for index in range(SCREEN_SIZE)]
     display(line, sleep_time)
 
@@ -56,14 +40,6 @@ def set_horizontal(sleep_time):
 def main():
     sleep_time = SLEEP_TIME
     while True:
-        # set_vertical()
-        # time.sleep(sleep_time)
-        # set_main_diagonal()
-        # time.sleep(sleep_time)
-        # set_horizontal()
-        # time.sleep(sleep_time)
-        # set_reverse_diagonal()
-        # time.sleep(sleep_time)
         set_vertical(sleep_time)
         set_main_diagonal(sleep_time)
         set_horizontal(sleep_time)
